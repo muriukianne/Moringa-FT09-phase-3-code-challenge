f7f8f8aa20d705b564b41fc8ed1148c1f35ac127
Anne Muriuki <muriukianne05@gmail.com>
2024-12-15 20:53:04 +0300

    add articles with database

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def main():
 
     '''
         The following is just for testing purposes,
-        you can modify it to meet the requirements of your implementation.
+        you can modify it to meet the requirements of your implmentation.
     '''
 
     # Create an author
@@ -29,7 +29,7 @@ def main():
     author_id = cursor.lastrowid  # Use this to fetch the id of the newly created author
 
     # Create a magazine
-    cursor.execute('INSERT INTO magazines (name, category) VALUES (?,?)', (magazine_name, magazine_category))
+    cursor.execute('INSERT INTO magazines (name, category) VALUES (?, ?)', (magazine_name, magazine_category))
     magazine_id = cursor.lastrowid  # Use this to fetch the id of the newly created magazine
 
     # Create an article
@@ -40,6 +40,7 @@ def main():
 
     # Query the database for inserted records.
     # The following fetch functionality should probably be in their respective models
+
     cursor.execute('SELECT * FROM magazines')
     magazines = cursor.fetchall()
 
@@ -54,26 +55,15 @@ def main():
     # Display results
     print("\nMagazines:")
     for magazine in magazines:
-        # Create Magazine object with name and category only
-        magazine_obj = Magazine(magazine[1], magazine[2])  # Magazine(name, category)
-        magazine_obj.id = magazine[0]  # Manually set the ID
-        print(f"ID: {magazine_obj.id}, Name: {magazine_obj.name}, Category: {magazine_obj.category}")
+        print(f"ID: {magazine[0]}, Name: {magazine[1]}, Category: {magazine[2]}")
 
     print("\nAuthors:")
     for author in authors:
-        # Create Author object with name only
-        author_obj = Author(author[1])  # Author(name)
-        author_obj.id = author[0]  # Manually set the ID
-        print(f"ID: {author_obj.id}, Name: {author_obj.name}")
+        print(f"ID: {author[0]}, Name: {author[1]}")
 
     print("\nArticles:")
     for article in articles:
-        # Create Article object, use Author and Magazine to link them
-        author_obj = Author(article[3])  # Fetch the author (ID 3 in the query result)
-        magazine_obj = Magazine(article[4])  # Fetch the magazine (ID 4 in the query result)
-        article_obj = Article(article[1], article[2], author_obj, magazine_obj)  # Article(title, content, author, magazine)
-        article_obj.id = article[0]  # Manually set the ID
-        print(f"ID: {article_obj.id}, Title: {article_obj.title}, Author: {article_obj.author().name}, Magazine: {article_obj.magazine().name}")
+        print(f"ID: {article[0]}, Title: {article[1]}, Content: {article[2]}, Author ID: {article[3]}, Magazine ID: {article[4]}")
 
 if __name__ == "__main__":
     main()
